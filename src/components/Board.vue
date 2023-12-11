67719fbbb6aacdd1cf3603fe02598c1c65059113
Stefan Kugele <stefan.kugele@gmail.com>
2023-12-11 02:16:23 +0100

    moving implemented

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -8,33 +8,55 @@ interface Square {
 }
 
 interface State {
-	squares: Square[]
+	squares: Array<Square>
 }
 
 const state = reactive<State>({
 	squares: []
 })
 
+let from = ''
+let to = ''
+
 const coord_squares = {
 	'files': ['a1', 'b1', 'c1', 'd1', 'e1', 'f1', 'g1', 'h1'],
 	'ranks': ['a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8']
 }
 
-onMounted(() => {
-		// Your mounted logic here
-		console.log('Component has been mounted');
-		fetch('http://localhost:9000/squaresJson')
-			.then(res => res.json())
-			.then(data => state.squares = data)
-			.catch(err => console.log(err.message))
-})
+function updateBoard() {
+	fetch('http://localhost:9000/squaresJson')
+		.then(res => res.json())
+		.then(data => state.squares = data)
+		.catch(err => console.log(err.message))
+}
+
+function move() {
+	fetch(`http://localhost:9000/game/play/move/${from}/${to}`)
+		.then(updateBoard)
+}
+
+function squareClick(e: MouseEvent) {
+	const square = e.target as HTMLElement
+	const coord = square.id
+	if (from === '') {
+		from = coord
+	}
+	else {
+		to = coord
+		move()
+		from = ''
+		to = ''
+	}
+}
+
+onMounted(updateBoard)
 </script>
 
 
 
 <template>
 	<div id="board">
-		<div v-for="square in state.squares" :id="square.coord" :class="square.color" class="square">
+		<div v-for="square in state.squares" :id="square.coord" :class="square.color" class="square" @click="squareClick">
 			<span class="piece">{{ square.piece }}</span>
 			<span v-if="coord_squares.files.includes(square.coord)" class="coord file">{{ square.coord[0] }}</span>
 			<span v-if="coord_squares.ranks.includes(square.coord)" class="coord rank">{{ square.coord[1] }}</span>
@@ -95,6 +117,7 @@ onMounted(() => {
 			z-index: 1;
 			font-size: 100cqh;
 			user-select: none;
+			pointer-events: none;
 		}
 	}
 	&::before {
