903d03176dd6c2a8f65ee0f1b53aa4774a3ee39a
Stefan Kugele <stefan.kugele@gmail.com>
2024-01-22 18:24:21 +0100

    vite pwa plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,24 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from "vite-plugin-pwa"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+		VitePWA({
+			manifest: {
+				icons: [
+					{
+						src: "/icons/512.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "any maskable"
+					}
+				]
+			}
+		})
   ],
   resolve: {
     alias: {
